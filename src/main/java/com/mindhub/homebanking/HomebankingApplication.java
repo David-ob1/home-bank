d94b03b1e49a22b212b7cd82100ff37d90b41bfe
David-ob1 <davidvazquezt2001@gmail.com>
2023-09-29 16:46:50 -0300

    version 1

---
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -0,0 +1,14 @@
+package com.mindhub.homebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//
+public class HomebankingApplication {
+	//esto es el main donde se trabaja
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+}
