279feacd1392ce6fd3f824896b2a05558154bbb6
David-ob1 <davidvazquezt2001@gmail.com>
2023-10-09 22:16:07 -0300

    Merge remote-tracking branch 'origin/rama2' into rama2

---
+++ src/main/resources/static/web/assets/account-transaction.js
@@ -0,0 +1,40 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        account:{},
+       transactions:{}
+      
+      }
+    },
+    created(){
+
+       let parametro = location.search
+        let params = new URLSearchParams(parametro)
+        console.log(params)
+
+        let idAccount = params.get("id")
+        console.log(idAccount)
+
+      // let urlParams = new URLSearchParams(location.search);
+      //   let id = urlParams.get('id')
+      
+     axios.get(`/api/accounts/${idAccount}`)
+      .then(response => {
+     
+        this.account = response.data;
+        console.log(this.account)
+
+        this.transactions = this.account.transactions
+
+        console.log(this.transactions)
+
+
+     })
+    }
+
+
+
+
+  }).mount('#app')
